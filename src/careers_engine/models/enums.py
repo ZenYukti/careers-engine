84481777160984cd44bd4d0ed255f3cbbeccc73b
Ayush <ayushhardeniya.grid@gmail.com>
2026-08-01 17:51:03 +0530

    feat(employment): normalize manual employment types (#4)

--- src/careers_engine/models/enums.py
+++ src/careers_engine/models/enums.py
@@ -3,6 +3,8 @@ from enum import Enum
 
 class EmploymentType(str, Enum):
     INTERN = "Internship"
+    NEW_GRAD = "New Grad"
+    APPRENTICESHIP = "Apprenticeship"
     FULL_TIME = "Full-time"
     PART_TIME = "Part-time"
     CONTRACT = "Contract"
